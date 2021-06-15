44743972c09de4c6406e15d2299abef5b18d985a
Poetic_Programmer <michaelgeorgedunleavy@gmail.com>
2021-06-15 09:02:49 +0100

    Added basic math and geometry data classes

---
+++ Shaper Tracer/Circle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shaper_Tracer
+{
+    class Circle
+    {
+        public readonly Vector2 CenterPosition;
+        public readonly float Radius;
+
+        public Circle(Vector2 centerPosition, float radius)
+        {
+            CenterPosition = centerPosition;
+            Radius = radius;
+        }
+    }
+}
